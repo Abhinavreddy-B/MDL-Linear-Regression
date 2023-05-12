1a6fa52a91e1136c65538d8f24c41edc36271742
Abhinav Reddy Boddu <abhinavreddyboddu@gmail.com>
2023-05-12 08:38:05 +0530

    to be uploaded to github

---
+++ zip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# create a directory
+rm 2021101034_assgn2.zip
+
+# move all files and directories to the directory
+zip 2021101034_assgn2.zip code.ipynb bonus.ipynb report.pdf readme.md *.pickle
